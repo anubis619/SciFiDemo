fa697a01093819b1105a798670ff6ed738cea04a
Alexandru Voinescu <marius.a.voinescu@gmail.com>
2019-01-20 19:02:40 +0200

    Added the looking mechanics and the player script

---
+++ Assets/Game/Scripts/LookY.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+
+    [SerializeField]
+    private float sensitivity = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x -= mouseY *sensitivity;
+        transform.localEulerAngles = newRotation;
+    }
+}
--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private CharacterController playercontroller;
+
+    // player speed variable
+    [SerializeField]
+    private float pSpeed = 3.5f;
+    //gravity
+    private float gravity = 9.81f;
+
     // Start is called before the first frame update
     void Start()
     {
+        playercontroller = GetComponent<CharacterController>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * pSpeed;
+        velocity.y -= gravity;
+        velocity = transform.transform.TransformDirection(velocity);
+        playercontroller.Move(velocity * Time.deltaTime);
     }
 }
